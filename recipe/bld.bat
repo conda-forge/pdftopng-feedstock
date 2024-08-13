723733ee5faf1ddbbc2f8101f12bb7eb63a87fbe
Uwe L. Korn <uwe.korn@quantco.com>
2024-08-13 14:30:15 +0200

    Different quoting

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,4 @@
-powershell -Command "(Get-Content setup.py) -replace 'std:c\+\+17', 'std:c\+\+20' | Set-Content setup.py"
+powershell -Command "(Get-Content setup.py) -replace 'std:c\+\+17', 'std:c++20' | Set-Content setup.py"
 
 copy numberofcharacters.h src\pdftopng
 
